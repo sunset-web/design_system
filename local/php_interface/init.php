25385e39e6a1b758bfc1887ad249caa00fecaa3a
sunset-web <ramos7691@gmail.com>
2023-09-15 15:04:25 +0300

    update structure

---
+++ local/php_interface/init.php
@@ -0,0 +1,7 @@
+<?
+//Классы проекта
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/classes.php');
+//Хелперы проекта
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/helpers/helpers.php');
+//Агенты проекта
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/agents/agents.php');
