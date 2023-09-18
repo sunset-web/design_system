30e7bafd4a59e4a011811b2c82b979a8c4a76ecc
Developer <ramos7691@gmail.com>
2023-09-18 11:51:20 +0300

    update loadpage

--- local/php_interface/classes/lib/LoadPage.php
+++ local/php_interface/classes/lib/LoadPage.php
@@ -5,14 +5,13 @@ class LoadPage
 {
     // Получение значения настроек
     function GetSettings()
-   {
-       global $arrMainSettings;
+    {
+        global $arrMainSettings;
 
-       if(\Bitrix\Main\Loader::includeModule('iblock'))
-        {
-                $arrMainSettings = \Bitrix\Iblock\Elements\ElementMainSettingsTable::getByPrimary(29, [
-                    'select' => ['TIME_' => 'TIME', 'EMAIL_' => 'EMAIL', 'ADDRESS_' => 'ADDRESS', 'PHONE_' => 'PHONE', ],
-                ])->fetch();
+        if (\Bitrix\Main\Loader::includeModule('iblock')) {
+            $arrMainSettings = \Bitrix\Iblock\Elements\ElementMainSettingsTable::getByPrimary(0, [
+                'select' => ['TIME_' => 'TIME', 'EMAIL_' => 'EMAIL', 'ADDRESS_' => 'ADDRESS', 'PHONE_' => 'PHONE',],
+            ])->fetch();
         }
-   }
+    }
 }
