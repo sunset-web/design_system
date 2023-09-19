f0cb67c6d1e21d4ce1e4d4e1e5db8191d4153511
Developer <ramos7691@gmail.com>
2023-09-19 11:39:24 +0300

    add comments and new helpers

--- local/php_interface/helpers/lib/debug.php
+++ local/php_interface/helpers/lib/debug.php
@@ -1,18 +1,32 @@
 <?
+
+/**
+ * Возвращает отформатированный контекст функцией var_dump
+ * @param $context - контекст для вывода
+ * @return string 
+ */
 function debug_var_dump($context)
 {
    echo '<pre>';
    var_dump($context);
    echo '</pre>';
 }
-
+/**
+ * Возвращает отформатированный контекст функцией print_r
+ * @param $context - контекст для вывода
+ * @return string 
+ */
 function debug_print_r($context)
 {
    echo '<pre>';
    print_r($context);
    echo '</pre>';
 }
-
+/**
+ * Возвращает отформатированный контекст функцией var_dump под гет параметром debug=y
+ * @param $context - контекст для вывода
+ * @return string 
+ */
 function debug_get_var_dump($context)
 {
    if ($_GET['debug'] = 'y') {
@@ -21,7 +35,11 @@ function debug_get_var_dump($context)
       echo '</pre>';
    }
 }
-
+/**
+ * Возвращает отформатированный контекст функцией print_r под гет параметром debug=y
+ * @param $context - контекст для вывода
+ * @return string 
+ */
 function debug_get_print_r($context)
 {
    if ($_GET['debug'] = 'y') {
@@ -30,7 +48,10 @@ function debug_get_print_r($context)
       echo '</pre>';
    }
 }
-
+/**
+ * Записывает контекст в журнал событий
+ * @param $context - контекст для вывода
+ */
 function debug_bitrix_log($context)
 {
    CEventLog::Add(array(
--- local/php_interface/helpers/lib/others.php
+++ local/php_interface/helpers/lib/others.php
@@ -1,12 +1,23 @@
 <?
-// форматирование телефона
+
+/**
+ * Возвращает отформатированное значени телефона
+ * @param $tel - телефон для форматирования
+ * @return string 
+ */
 function formatPhone($tel)
 {
    $kphone = preg_replace('/(.*), (.*)/', '$1', $tel);
    $kphone = str_replace([' ', '(', ')', '-'], '', $kphone);
    return $kphone;
 }
-// получение значения между строк
+/**
+ * Возвращает значение между строк
+ * @param $start - начало поиска
+ * @param $end - конец поиска
+ * @param $str - строка для поиска
+ * @return string 
+ */
 function getInbetweenStrings($start, $end, $str)
 {
    $matches = array();
@@ -14,7 +25,13 @@ function getInbetweenStrings($start, $end, $str)
    preg_match_all($regex, $str, $matches);
    return $matches[1];
 }
-// преобразование картинок
+/**
+ * Возвращает ссылку на преобразованную картинку
+ * @param $photo - id картинки из таблицы b_file, либо массив описание файла
+ * @param $width - ширина картинки
+ * @param $height - высота картинки
+ * @return string 
+ */
 function getResizePhoto($photo, $width, $height)
 {
    $file = CFile::ResizeImageGet(
@@ -25,7 +42,11 @@ function getResizePhoto($photo, $width, $height)
    );
    return $file['src'];
 }
-// Преобразование месяца
+/**
+ * Возвращает строковое значение месяца
+ * @param $num - число месяца
+ * @return string
+ */
 function formatMonth($num)
 {
    $arr = array(
@@ -43,4 +64,47 @@ function formatMonth($num)
       "12" => "декабря"
    );
    return $arr[$num];
+}
+/**
+ * Объект БитриксД7 _REQUEST
+ * @return mixed
+ */
+function getRequestObject()
+{
+   return \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
+}
+/**
+ * Возвращает объект менеджера управляемого кеша Bitrix D7
+ *
+ * @return object \Bitrix\Main\Data\ManagedCache
+ */
+function getCacheObj()
+{
+   return \Bitrix\Main\Application::getInstance()->getManagedCache();
+}
+/**
+ * Проверяет является ли входной параметром ботом
+ * @param $botname - название бота
+ * @return boolean
+ */
+function isBot(&$botname = '')
+{
+   $bots = array(
+      'rambler', 'googlebot', 'aport', 'yahoo', 'msnbot', 'turtle', 'mail.ru', 'omsktele',
+      'yetibot', 'picsearch', 'sape.bot', 'sape_context', 'gigabot', 'snapbot', 'alexa.com',
+      'megadownload.net', 'askpeter.info', 'igde.ru', 'ask.com', 'qwartabot', 'yanga.co.uk',
+      'scoutjet', 'similarpages', 'oozbot', 'shrinktheweb.com', 'aboutusbot', 'followsite.com',
+      'dataparksearch', 'google-sitemaps', 'appEngine-google', 'feedfetcher-google',
+      'liveinternet.ru', 'xml-sitemaps.com', 'agama', 'metadatalabs.com', 'h1.hrn.ru',
+      'googlealert.com', 'seo-rus.com', 'yaDirectBot', 'yandeG', 'yandex',
+      'yandexSomething', 'Copyscape.com', 'AdsBot-Google', 'domaintools.com',
+      'Nigma.ru', 'bing.com', 'dotnetdotcom'
+   );
+   foreach ($bots as $bot) {
+      if (stripos($_SERVER['HTTP_USER_AGENT'], $bot) !== false) {
+         $botname = $bot;
+         return true;
+      }
+   }
+   return false;
 }
