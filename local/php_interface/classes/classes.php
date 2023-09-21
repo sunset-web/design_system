0052eb35549259a7b562042e8723e4595061c32a
Developer <ramos7691@gmail.com>
2023-09-21 14:37:11 +0300

    update: edit structure

--- local/php_interface/classes/classes.php
+++ local/php_interface/classes/classes.php
@@ -1,30 +1,30 @@
 <?
-// Подключение началных настроек из ИБ на загрузку страницы
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/LoadPage.php');
-// метод модифицирует поисковый индекс для элементов и разделов инфоблока
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/SearchCatalog.php');
 // Создания лида в Б24 через отправку формы
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/CreateLeadB24.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/integrations/CreateLeadB24.php');
+// Подключение началных настроек из ИБ на загрузку страницы
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/events/LoadPage.php');
+// метод модифицирует поисковый индекс для элементов и разделов инфоблока
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/events/SearchCatalog.php');
 // Подмену логина на почту и телефон/ генерация автоматического логина
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/LoginPhoneMail.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/events/LoginPhoneMail.php');
 // Доп свойство наследуемое для элемента
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/CIBlockNewPropertySEO.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/CIBlockNewPropertySEO.php');
 // Доп свойство наследуемое для раздела
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/CIBlockNewPropertySectionSEO.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/CIBlockNewPropertySectionSEO.php');
 // Доп свойство для компонента main.feedback
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/SettingsPropertiyForm.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/SettingsPropertiyForm.php');
 // Доп свойство привязка к элементам с описанием
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/UpflyPropertiesList.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/UpflyPropertiesList.php');
 // Доп свойство привязка к цене
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/UpflyPriceFilter.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/UpflyPriceFilter.php');
 // Доп свойство перекрестные продажи
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/UpflyCrossSell.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/UpflyCrossSell.php');
 // Доп свойство условие фильтрации для перекрестных продаж
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/UpflyConditionMoreLess.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/UpflyConditionMoreLess.php');
 // Доп свойство склад для элементов
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/CIBlockNewPropertyStock.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/CIBlockNewPropertyStock.php');
 // Доп свойство склад для разделов
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/lib/CIBlockNewPropertyStockSection.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/props/CIBlockNewPropertyStockSection.php');
 
 //Load classes
 \Bitrix\Main\Loader::registerAutoLoadClasses(
